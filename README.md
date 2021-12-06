cf81360aa0885981b2220c69b72fded4139d7eb3
Travis Henning <henningta@gmail.com>
2021-12-06 02:30:03 -0500

    day 4 done, added readme.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Advent of Code 2021
+
+My code submissions for the [Advent of Code 2021](https://adventofcode.com/2021) daily challenges, written in Python.
+
+## Local development
+- Clone this repository and navigate to the project directory
+- Run `python -m venv venv` to set up the virtual environment (Python 3)
+- Run the appropriate activation scripts in the newly-created `venv` folder to activate the virtual environment in your terminal
+- Run `pip install -r requirements.txt` in the virtual environment
+- Execute each `.py` file in the `src/day*` folders individually, directly with Python
---
+++ src/day4/part2.py
@@ -0,0 +1,102 @@
+import numpy as np
+import os
+
+CLEAN_BINGO = np.zeros((5, 5), np.uint8)
+
+
+def build_boards(infile):
+    boards = []
+
+    board_lines = []
+    for line in infile:
+        parsed = [int(x) for x in line[:-1].split()]
+        board_lines.append(parsed)
+
+        if len(board_lines) == 5:
+            board = np.array((board_lines, CLEAN_BINGO))
+            # print(board)
+            boards.append(board)
+            board_lines = []
+            infile.readline()
+
+    return boards
+
+
+def check_bingo(board):
+    checked = board[1]
+    bingo = False
+
+    for row in checked:
+        if np.all(row == 1):
+            bingo = True
+            break
+
+    if not bingo:
+        for col in checked.T:
+            if np.all(col == 1):
+                bingo = True
+                break
+
+    if bingo:
+        actual = board[0]
+        unmarked = np.where(checked == 0)
+        unmarked_sum = np.sum(actual[unmarked])
+        return True, unmarked_sum
+    else:
+        return False, None
+
+
+def solve(infile):
+    infile.seek(0, 0)
+
+    # first line has the bingo numbers
+    bingo_numbers = [int(x) for x in infile.readline()[:-1].split(',')]
+    # print(bingo_numbers)
+
+    # build bingo boards
+    infile.readline()
+    boards = build_boards(infile)
+    # print(boards)
+
+    # bingo = False
+    unmarked_sum = None
+    number = None
+    finished_boards = {}
+
+    for i in range(len(bingo_numbers)):
+        number = bingo_numbers[i]
+
+        for board_index, board in enumerate(boards):
+            if board_index in finished_boards:
+                # board already won
+                continue
+
+            index = np.where(board[0] == number)
+
+            if len(index[0] == 1):
+                # number is in board
+                y, x = index[0][0], index[1][0]
+                board[1][y][x] = 1
+                bingo, _ = check_bingo(board)
+
+                if bingo:
+                    finished_boards[board_index] = True
+                    if len(finished_boards) == 100:
+                        # this is the last board to win
+                        bingo, unmarked_sum = check_bingo(board)
+                        return unmarked_sum * number
+
+    return None
+
+
+def main():
+    dirname = os.path.dirname(__file__)
+    filepath = os.path.join(dirname, 'input.txt')
+
+    with open(filepath, 'r') as infile:
+        solved = solve(infile)
+        print(solved)
+
+
+if __name__ == '__main__':
+    main()
